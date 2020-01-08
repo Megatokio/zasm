0fc947d617bfda7c04320cf7fd6b74b94e7a3c2f
kio <kio@little-bat.de>
2020-01-08 18:15:27 +0100

    pushed version to 4.2.6

--- Source/main.cpp
+++ Source/main.cpp
@@ -44,7 +44,7 @@
 
 
 //static const char appl_name[] = "zasm";
-static const char version[] = "4.2.5";
+static const char version[] = "4.2.6";
 
 // Hilfstext:
 // Anzeige optimiert für 80-Zeichen-Terminal
