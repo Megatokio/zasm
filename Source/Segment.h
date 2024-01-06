60a3b7da9132ebb49480615dd0ba95770990d900
kio <kio@little-bat.de>
2024-01-06 17:33:28 +0100

    update copyright year, replace copyright message with simple reference to BSD-2

--- Source/Segment.h
+++ Source/Segment.h
@@ -1,22 +1,8 @@
+// Copyright (c) 2014 - 2024 kio@little-bat.de
+// BSD-2-Clause license
+// https://opensource.org/licenses/BSD-2-Clause
+
 #pragma once
-/*	Copyright  (c)	Günter Woigk 2014 - 2023
-					mailto:kio@little-bat.de
-
-	This file is free software.
-
-	Permission to use, copy, modify, distribute, and sell this software
-	and its documentation for any purpose is hereby granted without fee,
-	provided that the above copyright notice appears in all copies and
-	that both that copyright notice, this permission notice and the
-	following disclaimer appear in supporting documentation.
-
-	THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT ANY WARRANTY,
-	NOT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR FITNESS FOR
-	A PARTICULAR PURPOSE, AND IN NO EVENT SHALL THE COPYRIGHT HOLDER
-	BE LIABLE FOR ANY DAMAGES ARISING FROM THE USE OF THIS SOFTWARE,
-	TO THE EXTENT PERMITTED BY APPLICABLE LAW.
-*/
-
 #include "SyntaxError.h"
 #include "Templates/RCObject.h"
 #include "Templates/RCPtr.h"
--- Source/assemble8080.cpp
+++ Source/assemble8080.cpp
@@ -1,20 +1,6 @@
-/*	Copyright  (c)	Günter Woigk 2014 - 2021
-					mailto:kio@little-bat.de
-
-	This file is free software.
-
-	Permission to use, copy, modify, distribute, and sell this software
-	and its documentation for any purpose is hereby granted without fee,
-	provided that the above copyright notice appears in all copies and
-	that both that copyright notice, this permission notice and the
-	following disclaimer appear in supporting documentation.
-
-	THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT ANY WARRANTY,
-	NOT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR FITNESS FOR
-	A PARTICULAR PURPOSE, AND IN NO EVENT SHALL THE COPYRIGHT HOLDER
-	BE LIABLE FOR ANY DAMAGES ARISING FROM THE USE OF THIS SOFTWARE,
-	TO THE EXTENT PERMITTED BY APPLICABLE LAW.
-*/
+// Copyright (c) 2014 - 2024 kio@little-bat.de
+// BSD-2-Clause license
+// https://opensource.org/licenses/BSD-2-Clause
 
 #include "Z80/goodies/z80_opcodes.h"
 #include "Z80Assembler.h"
